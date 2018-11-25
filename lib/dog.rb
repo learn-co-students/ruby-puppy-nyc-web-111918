1230ad7cd26dfb6ec11ef302497b5588bd45cb95
MichaelM3 <dagreekarican@aim.com>
2018-11-25 17:33:05 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
  class Dog
-   
+   attr_reader :name
+
+   @@all = []
+
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
+
+   def self.clear_all
+     @@all.clear
+   end
+
+   def self.all
+    for i in 0...@@all.length
+      puts @@all[i].name
+    end
+   end
  end
